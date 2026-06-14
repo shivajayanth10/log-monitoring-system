3f21234dacb71a3bb9c604a31d8c3dee74ec8bde
shivajayanth10 <shivjayanth999@gmail.com>
2026-06-14 23:57:31 +0530

    Add load_logs_from_file function

--- load_logs.py
+++ load_logs.py
@@ -1,18 +1,19 @@
 from log_parser import parse_logs
 from database import create_database, insert_log
 
-# Create database and table
-create_database()
+def load_logs_from_file(filename):
+    # Create database and table
+    create_database()
 
-# Read logs from sample.log
-logs = parse_logs("sample.log")
+    # Read logs from file
+    logs = parse_logs(filename)
 
-# Insert each log into database
-for log in logs:
-    insert_log(
-        log["timestamp"],
-        log["level"],
-        log["message"]
-    )
+    # Insert each log into database
+    for log in logs:
+        insert_log(
+            log["timestamp"],
+            log["level"],
+            log["message"]
+        )
 
-print("Logs loaded successfully!")
+    print("Logs loaded successfully!")
